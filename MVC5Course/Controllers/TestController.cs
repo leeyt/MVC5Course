5be840f548728f3729ef420cba99475d7160c2bf
Neil Lee <neil.lee@udngroup.com.tw>
2017-12-02 17:33:35 +0800

    建立新的 TestController 並練習透過 Entity Framework 操作 CRUD 動作

--- MVC5Course/Controllers/TestController.cs
+++ MVC5Course/Controllers/TestController.cs
@@ -1,17 +1,88 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace MVC5Course.Controllers
+﻿namespace MVC5Course.Controllers
 {
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Web.Mvc;
+
+    using MVC5Course.Models;
+
     public class TestController : Controller
     {
+        private readonly FabricsEntities db = new FabricsEntities();
+
         // GET: Test
         public ActionResult Index()
         {
+            var data = from p in db.Product
+                       select p;
+
+            return View(data.Take(10));
+        }
+
+        public ActionResult Create()
+        {
+
             return View();
         }
+
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult Create(Product data)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Product.Add(data);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(data);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var item = db.Product.Find(id);
+
+            return View(item);
+        }
+
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Product data)
+        {
+            if (ModelState.IsValid)
+            {
+                var item = db.Product.Find(id);
+
+                item.ProductName = data.ProductName;
+                item.Price = data.Price;
+                item.Stock = data.Stock;
+
+                db.SaveChanges();
+                
+                return RedirectToAction("Index");
+            }
+
+            return View(data);
+        }
+
+        public ActionResult Details()
+        {
+            return View(db.Product.Take(10));
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var product = db.Product.Find(id);
+
+            db.OrderLine.RemoveRange(product.OrderLine.ToList());
+
+            db.Product.Remove(product);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
