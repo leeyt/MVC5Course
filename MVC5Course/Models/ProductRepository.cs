4dbbc283d864acdc0645c21ca8070a510823dcc0
Neil Lee <neil.lee@udngroup.com.tw>
2017-12-03 12:37:35 +0800

    透過覆寫 All() 方法套用過濾刪除資料的邏輯

--- MVC5Course/Models/ProductRepository.cs
+++ MVC5Course/Models/ProductRepository.cs
@@ -4,6 +4,11 @@ namespace MVC5Course.Models
 
     public class ProductRepository : EFRepository<Product>, IProductRepository
     {
+        public override IQueryable<Product> All()
+        {
+            return base.All().Where(p => !p.IsDeleted);
+        }
+
         public Product Find(int id)
         {
             return this.All().FirstOrDefault(p => p.ProductId == id);
@@ -11,7 +16,7 @@ namespace MVC5Course.Models
 
         public IQueryable<Product> Get取得所有尚未刪除的商品資料()
         {
-            return this.All().Where(p => !p.IsDeleted);
+            return this.All();
         }
 
         public IQueryable<Product> Get取得所有尚未刪除的商品資料Top10()
