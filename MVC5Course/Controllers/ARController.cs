c56038879d2a4909720b42b2cddc7b21c4a070ff
Neil Lee <neil.lee@udngroup.com.tw>
2017-12-03 15:36:58 +0800

    1. 練習 ViewResult 與 PartialViewResult 的使用方式 2. 實作 JsAlertRedirect 共用檢視 (Shared View)

---
+++ MVC5Course/Controllers/ARController.cs
@@ -0,0 +1,27 @@
+﻿namespace MVC5Course.Controllers
+{
+    using System.Web.Mvc;
+    public class ARController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PartialViewTest()
+        {
+            return PartialView("Index");
+        }
+
+        public ActionResult ContentTest()
+        {
+            // 這樣寫會導致維護性降低，不太方便管理!
+            return Content("<script>alert('OK'); location.href='/';</script>");
+        }
+
+        public ActionResult ContentTest_Better()
+        {
+            return PartialView("JsAlertRedirect", "新增成功");
+        }
+    }
+}
