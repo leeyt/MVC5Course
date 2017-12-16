57ffdca5c78e17a170b8112c553c77da94260492
Neil Lee <neil.lee@udngroup.com.tw>
2017-12-16 15:08:44 +0800

    練習 /MB/Index 頁面的批次更新功能 ( 使用多筆資料的模型繫結 )

--- MVC5Course/Controllers/HomeController.cs
+++ MVC5Course/Controllers/HomeController.cs
@@ -1,22 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using MVC5Course.ActionFilters;
 
 namespace MVC5Course.Controllers
 {
+    [LocalOnly]
     public class HomeController : Controller
     {
+        [ShareData]
         public ActionResult Index()
         {
             return View();
         }
 
+        [ShareData]
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
--- MVC5Course/Controllers/MBController.cs
+++ MVC5Course/Controllers/MBController.cs
@@ -1,6 +1,15 @@
 ﻿namespace MVC5Course.Controllers
 {
+    using System;
     using System.Web.Mvc;
+
+    public class MBBatchUpdateVM
+    {
+        public int ProductId { get; set; }
+        public Nullable<decimal> Price { get; set; }
+        public Nullable<bool> Active { get; set; }
+        public Nullable<decimal> Stock { get; set; }
+    }
 
     public class MBController : BaseController
     {
@@ -12,6 +21,30 @@
 
             ViewBag.PageTitle = "商品資料";          
 
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(MBBatchUpdateVM[] batch)
+        {
+            if (ModelState.IsValid)
+            {
+                foreach (var item in batch)
+                {
+                    var one = repo.Find(item.ProductId);
+                    one.Price = item.Price;
+                    one.Active = item.Active;
+                    one.Stock = item.Stock;
+                }
+
+                repo.UnitOfWork.Commit();
+
+                return RedirectToAction("Index");
+            }
+
+            var data = repo.Get取得所有尚未刪除的商品資料Top10();
+            ViewData.Model = data;
+            ViewBag.PageTitle = "商品清單";
             return View();
         }
     }
