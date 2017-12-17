f504c5b1d1dea4ffb04c34a9e638c06ed67223f1
Neil Lee <neil.lee@udngroup.com.tw>
2017-12-17 11:50:54 +0800

    練習使用 @Html.Action 載入另一個 Action 的執行結果

---
+++ MVC5Course/Controllers/OrderLinesController.cs
@@ -0,0 +1,34 @@
+﻿
+namespace MVC5Course.Controllers
+{
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Net;
+    using System.Web.Mvc;
+
+    using MVC5Course.Models;
+
+    public class OrderLinesController : Controller
+    {
+        private FabricsEntities db = new FabricsEntities();
+
+        // GET: OrderLines
+        public ActionResult Index(int id)
+        {
+            var orderLine = db.OrderLine
+                .Where(p => p.ProductId == id)
+                .Include(o => o.Order)
+                .Include(o => o.Product);
+            return View(orderLine.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
